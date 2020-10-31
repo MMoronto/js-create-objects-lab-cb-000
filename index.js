944a488f9035f2e737cee94c8ef019128ea141cc
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-10-31 07:54:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 'use strict';
+function Animal(species, name, breed, age, foodRequirements, waterConditions) {
+  this.species = species;
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+  this.foodRequirements = foodRequirements;
+  this.waterConditions = waterConditions;
+}
+
