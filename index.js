d276845799f441629c1f2c4b2679dab7e45031be
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-10-31 08:03:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+function Dog(name, breed, age) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Cat(name, breed, age) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Dog(species, name, breed, age, foodRequirements, waterConditions) {
+  this.species = species;
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+  this.foodRequirements = foodRequirements;
+  this.waterConditions = waterConditions;
+}
+
 function Dog(species, name, breed, age, foodRequirements, waterConditions) {
   this.species = species;
   this.name = name;
